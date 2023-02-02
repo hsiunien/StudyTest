1bc2d4c6bf82b0f566ff50e0c7af1048b318f2af
hugh <zens53@163.com>
2023-02-02 11:56:32 +0800

    update fieldof view for camera make it look like close to the target

--- Unity/CameraMoving/Assets/ClickHandler.cs
+++ Unity/CameraMoving/Assets/ClickHandler.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClickHandler : MonoBehaviour
@@ -7,12 +9,42 @@ public class ClickHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DOTween.Init();
     }
 
     // Update is called once per frame
     void Update()
+    { 
+            if (Input.GetMouseButtonDown(0))
+            {
+               
+            }
+            
+            if(Input.GetMouseButtonUp(0))
+            {
+    
+                castRay(Input.mousePosition);
+            }
+ 
+    }
+    private void castRay(Vector3 position)
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject == this.gameObject)
+            {
+                OnClicked();
+            }
+        }
+ 
+    }
+
+    private void OnClicked()
+    {
+        Debug.Log($"clicked,{Camera.main.depth}, {Camera.main.fieldOfView}");
+        DOTween.To(() => Camera.main.fieldOfView, x => Camera.main.fieldOfView = x, 20f, 1);
     }
 }
