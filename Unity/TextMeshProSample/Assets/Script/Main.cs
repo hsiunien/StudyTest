5f1c9f3fe8c87b2acf74d9780c9f0b3d98a97728
hugh <zens53@163.com>
2023-02-14 21:14:16 +0800

    unity: init text mesh pro

---
+++ Unity/TextMeshProSample/Assets/Script/Main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public TextMeshProUGUI textUGUI;
+    void Start()
+    {
+        textUGUI.text = "emogi test😄💐";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
